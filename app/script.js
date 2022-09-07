912b69ff030ac18405b9918bb68174266dbef174
Александр <zinovyevaleks@yandex.ru>
2022-09-08 00:46:39 +0700

    error display and scroll

--- app/script.js
+++ app/script.js
@@ -3,24 +3,41 @@
 const testForm = document.querySelector(".test__table");
 const formRows = testForm.querySelectorAll(".questions__body .questions__row");
 const checkboxes = testForm.querySelectorAll(".form__checkbox-input");
+const errorMessage = document.querySelector(".test__error");
+const scrollToButton = document.querySelector(".test__error-scroll");
 
 function handleFormSubmit(event) {
   formRows.forEach((row) => {
     if (!row.querySelector(".form__checkbox-input:checked")) {
       event.preventDefault();
       row.classList.add("isError");
-      console.log("error");
-    } else {
-      console.log("Отправка");
+      errorMessage.classList.add("test__error--shown");
     }
   });
 }
 
+function checkIsErrors() {
+  if (!document.querySelectorAll(".isError").length) {
+    errorMessage.classList.remove("test__error--shown");
+  }
+}
+
 checkboxes.forEach((checkbox) => {
   checkbox.addEventListener("change", () => {
-    console.log("change");
     checkbox.closest(".questions__row").classList.remove("isError");
+    checkIsErrors();
   });
 });
 
 testForm.addEventListener("submit", handleFormSubmit);
+
+scrollToButton.addEventListener("click", () => {
+  const firstError = document.querySelector(".isError");
+  firstError.scrollIntoView({ behavior: "smooth", block: "center" });
+  setTimeout(() => {
+    firstError.classList.add('isError--animated');
+  }, 1000);
+  setTimeout(() => {
+    firstError.classList.remove('isError--animated');
+  }, 1500);
+});
--- dist/script.js
+++ dist/script.js
@@ -3,23 +3,42 @@
 var testForm = document.querySelector(".test__table");
 var formRows = testForm.querySelectorAll(".questions__body .questions__row");
 var checkboxes = testForm.querySelectorAll(".form__checkbox-input");
+var errorMessage = document.querySelector(".test__error");
+var scrollToButton = document.querySelector(".test__error-scroll");
 
 function handleFormSubmit(event) {
   formRows.forEach(function (row) {
     if (!row.querySelector(".form__checkbox-input:checked")) {
       event.preventDefault();
       row.classList.add("isError");
-      console.log("error");
-    } else {
-      console.log("Отправка");
+      errorMessage.classList.add("test__error--shown");
     }
   });
 }
 
+function checkIsErrors() {
+  if (!document.querySelectorAll(".isError").length) {
+    errorMessage.classList.remove("test__error--shown");
+  }
+}
+
 checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
-    console.log("change");
     checkbox.closest(".questions__row").classList.remove("isError");
+    checkIsErrors();
   });
 });
 testForm.addEventListener("submit", handleFormSubmit);
+scrollToButton.addEventListener("click", function () {
+  var firstError = document.querySelector(".isError");
+  firstError.scrollIntoView({
+    behavior: "smooth",
+    block: "center"
+  });
+  setTimeout(function () {
+    firstError.classList.add('isError--animated');
+  }, 1000);
+  setTimeout(function () {
+    firstError.classList.remove('isError--animated');
+  }, 1500);
+});
