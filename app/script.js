3b9b5819cf983b3e0169e24fd5797aae4da6af4b
Александр <zinovyevaleks@yandex.ru>
2022-09-07 23:19:34 +0700

    form validation

--- app/script.js
+++ app/script.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+
+const testForm = document.querySelector(".test__table");
+const formRows = testForm.querySelectorAll(".questions__body .questions__row");
+const checkboxes = testForm.querySelectorAll(".form__checkbox-input");
+
+function handleFormSubmit(event) {
+  formRows.forEach((row) => {
+    if (!row.querySelector(".form__checkbox-input:checked")) {
+      event.preventDefault();
+      row.classList.add("isError");
+      console.log("error");
+    } else {
+      console.log("Отправка");
+    }
+  });
+}
+
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", () => {
+    console.log("change");
+    checkbox.closest(".questions__row").classList.remove("isError");
+  });
+});
+
+testForm.addEventListener("submit", handleFormSubmit);
--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+
+var testForm = document.querySelector(".test__table");
+var formRows = testForm.querySelectorAll(".questions__body .questions__row");
+var checkboxes = testForm.querySelectorAll(".form__checkbox-input");
+
+function handleFormSubmit(event) {
+  formRows.forEach(function (row) {
+    if (!row.querySelector(".form__checkbox-input:checked")) {
+      event.preventDefault();
+      row.classList.add("isError");
+      console.log("error");
+    } else {
+      console.log("Отправка");
+    }
+  });
+}
+
+checkboxes.forEach(function (checkbox) {
+  checkbox.addEventListener("change", function () {
+    console.log("change");
+    checkbox.closest(".questions__row").classList.remove("isError");
+  });
+});
+testForm.addEventListener("submit", handleFormSubmit);
